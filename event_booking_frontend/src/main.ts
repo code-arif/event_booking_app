ab195d3974f51d796287c17035446ee55727f25e
code-arif <arifulislam6460@gmail.com>
2025-03-05 15:49:39 +0600

    api done

--- event_booking_frontend/src/main.ts
+++ event_booking_frontend/src/main.ts
@@ -3,4 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 createApp(App).mount('#app')
